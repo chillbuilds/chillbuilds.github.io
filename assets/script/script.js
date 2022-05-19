a6b31a10af321698c290cc835fdfbda789a6a453
Will Wojeck <will.wojeck@leafgroup.com>
2022-05-18 20:23:08 -0600

    moved margin calculation function after window/document load

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,5 @@
 $(window).on('resize', () => {
-    let winHeight = $(window).height()
-    let cardDivHeight = parseInt($('.home-card').height())*2
-    let topMargin = parseInt((winHeight - cardDivHeight)/2)
-
-    console.log('window height: ' + winHeight)
-    console.log('card div height: ' + cardDivHeight)
-    console.log('margin: ' + parseInt((winHeight - cardDivHeight)/2))
-
-    $('#body-container').attr('style', `margin-top: ${topMargin-48}px;`)
+    marginCalc()
 })
 
 function marginCalc() {
@@ -26,4 +18,6 @@ $('.home-card').on('click', function(){
     window.location.href = `/${$(this).attr('id')}`;
 })
 
-marginCalc()
+$(document).ready(function() {
+    marginCalc()
+})
