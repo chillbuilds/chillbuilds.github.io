05306ae1b63f00e27e75254fc2f18c7101953c69
Will Wojeck <will.wojeck@leafgroup.com>
2022-05-18 20:55:42 -0600

    added image tag text

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,14 +4,15 @@ $(window).on('resize', () => {
 
 function marginCalc() {
     let winHeight = $(window).height()
-    let cardDivHeight = parseInt($('.home-card').height())*2
-    let topMargin = parseInt((winHeight - cardDivHeight)/2)
+    let cardDivHeight = $('.home-card').height()*2
+    let topMargin = (winHeight - cardDivHeight)/2
 
     console.log('window height: ' + winHeight)
     console.log('card div height: ' + cardDivHeight)
     console.log('margin: ' + parseInt((winHeight - cardDivHeight)/2))
 
     $('#body-container').attr('style', `margin-top: ${topMargin-48}px;`)
+    $('.card-text').attr('style', `top: ${cardDivHeight/2-48}px`)
 }
 
 $('.home-card').on('click', function(){
