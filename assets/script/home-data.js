06be8c88ef0a5cdcccfa1d3f2add49400b8e772e
Will Wojeck <willwojeck@Will-Wojeck.local>
2023-04-01 23:12:57 -0600

    updating page layout. project cards are still fucked.

---
+++ assets/script/home-data.js
@@ -0,0 +1,86 @@
+let dataObj = [
+    {
+        title: 'stamp',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'open back headphones',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'grow tent',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'raspi battlebot',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'dabbling with flexAR and an implanted magnet',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'soap mold making',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'portable reel to reel retrofit',
+        img_src: '',
+        body: '',
+        live: 'n'
+    },
+    {
+        title: 'closet garden',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'mold making for pewter casting',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'hexagonal ice tray',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'electric skateboard',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'cat candle making',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: 'deadbolt actuation',
+        img_src: '',
+        body: '',
+        live: 'y'
+    },
+    {
+        title: '',
+        img_src: '',
+        body: '',
+        live: 'n'
+    },
+]
